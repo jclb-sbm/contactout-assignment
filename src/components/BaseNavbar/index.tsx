4e5dc5fdea3cada3cc048abf5225d55979c9835b
John Christian Los Bañes <johnchristian@kingslanduniversity.com>
2021-08-08 00:51:05 +0800

    feat(NavBar) add Avatar Component

--- src/components/BaseNavbar/index.tsx
+++ src/components/BaseNavbar/index.tsx
@@ -1,10 +1,19 @@
 import './BaseNavbar.sass';
 import {
-  Container,
   Nav,
   Navbar,
 } from 'react-bootstrap'
 import { useMediaQuery } from 'react-responsive'
+
+function Avatar(props: any) {
+  return (
+    <img
+      className={props.className}
+      src="/wozniak.jpg"
+      alt="Navbar Avatar"
+    />
+  )
+}
 
 function NavbarLogo(props: any) {
   return (
@@ -12,9 +21,33 @@ function NavbarLogo(props: any) {
       className={props.className}
       src="/sample-logo.png"
       alt="Navbar Logo"
-      width="50px"
-      height="50px"
     />
+  )
+}
+
+function TopNavbar(props: any) {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="topnav">
+      <Navbar.Brand href="/">
+        <NavbarLogo className="topnav__logo"/>
+      </Navbar.Brand>
+      <div className="d-flex flex-row">
+        <Nav.Link href="/">
+          <Avatar className="topnav__avatar"/>
+        </Nav.Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"  className="my-2" />
+      </div>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          {props.links.map((link: any) => 
+            <Nav.Link
+              key={`top-${link.url}${link.label}`}
+              href={link.url}>{link.label}
+            </Nav.Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
@@ -30,38 +63,20 @@ function SideNavbar(props: any) {
           key={`side-${link.url}${link.label}`} 
           href={link.url}
         >
-          <div>
-            <i className={`bi ${link.icon} sidenav__icon`}></i>
-            <div>
-              {link.label}
-            </div>
-          </div>
+          <i className={`bi ${link.icon} sidenav__icon`}></i>
+          <div>{link.label}</div>
         </Nav.Link>
       )}
+      <div className="mt-auto sidenav__avatar-container">
+        <Nav.Link href="/">
+          <Avatar className="sidenav__avatar"/>
+        </Nav.Link>
+      </div>
     </Nav>
   )
 }
 
-function TopNavbar(props: any) {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home"><NavbarLogo/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {props.links.map((link: any) => 
-              <Nav.Link
-                key={`top-${link.url}${link.label}`}
-                href={link.url}>{link.label}
-              </Nav.Link>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
-}
+
 
 function BaseNavbar(props: any) {
   const isLgScreen = useMediaQuery({ query: '(min-width: 786px)' })
