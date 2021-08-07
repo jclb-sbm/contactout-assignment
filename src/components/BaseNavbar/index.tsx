5d4402812d82bb91c05cd7b2c49fdf997fa7e18e
John Christian Los Bañes <johnchristian@kingslanduniversity.com>
2021-08-07 19:06:53 +0800

    feat(Navbar): create Navbar Logo Component

--- src/components/BaseNavbar/index.tsx
+++ src/components/BaseNavbar/index.tsx
@@ -6,14 +6,27 @@ import {
 } from 'react-bootstrap'
 import { useMediaQuery } from 'react-responsive'
 
+function NavbarLogo(props: any) {
+  return (
+    <img
+      className={props.className}
+      src="/sample-logo.png"
+      alt="Navbar Logo"
+      width="50px"
+      height="50px"
+    />
+  )
+}
+
 function SideNavbar(props: any) {
   return (
-    <Nav 
-      className="flex-column bg-dark sidenav"
-    >
+    <Nav className="flex-column bg-dark sidenav">
+      <Nav.Link href="/">
+        <NavbarLogo className="sidenav__logo mt-4"/>
+      </Nav.Link>
       {props.links.map((link: any) => 
         <Nav.Link 
-          className="text-white text-center"
+          className="text-white text-center mt-2"
           key={`side-${link.url}${link.label}`} 
           href={link.url}
         >
@@ -33,7 +46,7 @@ function TopNavbar(props: any) {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><NavbarLogo/></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
